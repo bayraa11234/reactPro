eca03bc4393c0e3929130c85418d75f5fd99adc2
odbayar <odbayardorjsuren8@gmail.com>
2023-02-01 16:49:34 +0800

    main

--- snake-react/src/App.js
+++ snake-react/src/App.js
@@ -51,7 +51,8 @@ const [direction, setDirection]= useState('Down');
     let newSnake = [...snake];
     newSnake = newSnake.map((item, index) =>{
       if(index === 0 ){
-      return {x: item.x, y: item.y+1};
+        if(item.y === yCells - 1) return {x: item.x, y: 0};
+        return {x: item.x, y: item.y+1};
       }
       return {x: newSnake[index - 1].x, y: newSnake[index - 1].y};
     });
@@ -62,6 +63,7 @@ const [direction, setDirection]= useState('Down');
     let newSnake = [...snake];
     newSnake = newSnake.map((item, index) =>{
       if(index === 0 ){
+        if(item.x === yCells - 1) return {x: 0, y: item.y};
       return {x:item.x + 1, y: item.y};
       }
       return {x: newSnake[index - 1].x, y: newSnake[index - 1].y};
@@ -73,6 +75,7 @@ const [direction, setDirection]= useState('Down');
     let newSnake = [...snake];
     newSnake = newSnake.map((item, index) =>{
       if(index === 0 ){
+        if(item.y === 0) return {x: 0, y: yCells - 1};
       return {x:item.x, y: item.y-1};
       }
       return {x: newSnake[index - 1].x, y: newSnake[index - 1].y};
@@ -84,6 +87,7 @@ const [direction, setDirection]= useState('Down');
     let newSnake = [...snake];
     newSnake = newSnake.map((item, index) =>{
       if(index === 0 ){
+        if(item.x === 0) return {x: xCells - 1 , y: item.y};
       return {x:item.x - 1, y: item.y};
       }
       return {x: newSnake[index - 1].x, y: newSnake[index - 1].y};
